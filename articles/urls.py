705b7c920d733edba13cbb3719badeb461f1c3b1
Pratik <prabhupratik27@gmail.com>
2018-12-24 17:05:40 +0530

    Completed 24

--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,6 @@ app_name = 'articles'
 
 urlpatterns = [
     path('',views.articlelist,name="list"),
+    path('create/',views.create_article,name="create"),
     path('<slug:title>',views.article_detail,name="detail"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='accounts:login')
 def articlelist(request):
 	articles = Article.objects.all().order_by('date')
 	return render(request,'articles/article_list.html',{'articles':articles})
 
+@login_required(login_url='accounts:login')
 def article_detail(request,title):
 	article  = Article.objects.get(slug=title)
 	#return HttpResponse(title)
 	return render(request,'articles/article_detail.html',{'article':article})
+
+@login_required(login_url='accounts:login')
+def create_article(request):
+	return render(request,'articles/article_create.html')
 
